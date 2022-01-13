755a89ef743a6c1e79423183919bf199a4702458
Bagus Rinu Pangayom <bagusrp2001@gmail.com>
2022-01-13 09:52:54 +0700

    pembaharuan ke-2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function onSignIn(googleUser) {
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
-        alert("You have been signed out successfully");
+        alert("Sign Out Telah Berhasil");
         $(".data").css("display", "none");
         $(".g-signin2").css("display", "block");
     });
